39f024bfd78b2df7c643b555517509005e3a373c
mahimX <112805408+69mahimX@users.noreply.github.com>
2022-09-04 04:29:40 -0700

    Add files via upload

---
+++ hi.py
@@ -0,0 +1,25 @@
+
+import colorama
+from colorama import Fore ,Back,Style
+
+import pyfiglet 
+
+print(" tool by")
+print(pyfiglet.figlet_format("mahimX"))
+
+print("i gonna wriHgt a love letter",",ooh sorry love script")
+
+
+n= input(" enter name of who you love  ")
+print("i can't under stand what do i wright "," i just thinking about it","ooh",n,"i can't say \n how long im trying to wright a love letter")
+
+print("but",n,"i failed to wright a love letter.","beacuse hackers are not for wrighting love letters \n but",n,"as a newb hacker i can wright python proggraming scrip\n ")
+
+print(" so what can i do without it??","\n actually i can do but","whos gonna make stupid and a classic love letter ","only the normal general peoples who dont know abut the computers the wright love letters"," i can wright a love litter script "," do you know what is python??","it not just a proggramming language ","\n this is the most advanced programming language for hackers")
+
+print(n,"maybe you can't understand what im talking about ","but,",n,"you haven't understand",'you have to understad what im talking'," i just wanna say that ")
+print(" i love you")
+print(pyfiglet.figlet_format(n))
+
+print("i don't know why i love you",",but i love you ")
+
